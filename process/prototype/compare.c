d6c8baa89126ed58061ad5664e254cd2f291f89f
Captdam <captdam@beardle.com>
2021-05-17 04:21:45 -0400

    New compare algorithm, better accuracy

--- process/prototype/compare.c
+++ process/prototype/compare.c
@@ -6,8 +6,9 @@
 
 #include "compare.h"
 
-#define LUMA_THRESHOLD 10
+#define LUMA_THRESHOLD 40
 #define DISTANCE_THRESHOLD 100
+#define SEARCH_REGION 10
 
 struct Compare_ClassDataStructure {
 	size2d_t size;
@@ -54,8 +55,11 @@ void compare_setLocationMap(Compare this, loc3d_t* location) {
 	memcpy(this->location, location, sizeof(loc3d_t) * this->size.width * this->size.height);
 }
 
-size_t distanceLoc3d(loc3d_t a, loc3d_t b) {
+float distanceLoc3d(loc3d_t a, loc3d_t b) {
 	return sqrt( powf(a.x-b.x,2) + powf(a.y-b.y,2) + powf(a.z-b.z,2) );
+}
+uint8_t clamp(float i) {
+	return (i > 255) ? 255 : i;
 }
 void compare_process(Compare this) {
 	memset(this->speed, 0, sizeof(uint8_t) * this->size.width * this->size.height);
@@ -64,6 +68,27 @@ void compare_process(Compare this) {
 		for (size_t x = 0; x < this->size.width; x++) {
 			size_t indexSelf = y * this->size.width + x;
 			if (this->projectSource[indexSelf] > LUMA_THRESHOLD) {
+				float min = 999999999.9f;
+				size_t l = x > SEARCH_REGION ? x - SEARCH_REGION : 0;
+				size_t r = x < this->size.width - SEARCH_REGION ? x + SEARCH_REGION : this->size.width;
+				size_t u = y > SEARCH_REGION ? y - SEARCH_REGION : 0;
+				size_t d = y < this->size.height - SEARCH_REGION ? y + SEARCH_REGION : this->size.height;
+				for (size_t sy = u; sy < d; sy++) {
+					for (size_t sx = l; sx < r; sx++) {
+						if (abs(sx-x) < 2 || abs(sy-y) < 2)
+							continue;
+						size_t indexSearch = sy * this->size.width + sx;
+						if (this->projectBuffer[indexSearch] > LUMA_THRESHOLD) {
+							float d = distanceLoc3d(this->location[indexSelf], this->location[indexSearch]);
+							if (d < min)
+								min = d;
+						}
+					}
+				}
+				this->speed[indexSelf] = clamp( this->ratio * d );
+			}
+/*			size_t indexSelf = y * this->size.width + x;
+			if (this->projectSource[indexSelf] > LUMA_THRESHOLD) {
 				for (size_t d = 0; d < DISTANCE_THRESHOLD; d++) {
 					size_t indexLeft = indexSelf - d;
 					size_t indexRight = indexSelf + d;
@@ -71,26 +96,26 @@ void compare_process(Compare this) {
 					size_t indexDown = indexSelf + d * this->size.width;
 					if (this->projectBuffer[indexLeft] > LUMA_THRESHOLD) {
 						if (d > 1) //Noise when distance = 1
-							this->speed[indexSelf] = this->ratio * distanceLoc3d(this->location[indexSelf], this->location[indexLeft]);
+							this->speed[indexSelf] = clamp( this->ratio * distanceLoc3d(this->location[indexSelf], this->location[indexLeft]) );
 						break;
 					}
 					if (this->projectBuffer[indexRight] > LUMA_THRESHOLD) {
 						if (d > 1)
-							this->speed[indexSelf] = this->ratio * distanceLoc3d(this->location[indexSelf], this->location[indexRight]);
+							this->speed[indexSelf] = clamp( this->ratio * distanceLoc3d(this->location[indexSelf], this->location[indexRight]) );
 						break;
 					}
 					if (this->projectBuffer[indexUp] > LUMA_THRESHOLD) {
 						if (d > 1)
-							this->speed[indexSelf] = this->ratio * distanceLoc3d(this->location[indexSelf], this->location[indexUp]);
+							this->speed[indexSelf] = clamp( this->ratio * distanceLoc3d(this->location[indexSelf], this->location[indexUp]) );
 						break;
 					}
 					if (this->projectBuffer[indexDown] > LUMA_THRESHOLD) {
 						if (d > 1)
-							this->speed[indexSelf] = this->ratio * distanceLoc3d(this->location[indexSelf], this->location[indexDown]);
+							this->speed[indexSelf] = clamp( this->ratio * distanceLoc3d(this->location[indexSelf], this->location[indexDown]) );
 						break;
 					}
 				}
-			}
+			}*/
 		}
 	}
 
