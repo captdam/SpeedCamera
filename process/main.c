669880603d3a91ab7c778a8563c7ad1cf9206f4e
Captdam <captdam@beardle.com>
2021-09-02 05:20:37 -0400

    Update main: - Add roadmap - Bug fix: {} causes variable memory space reuse and override

--- process/main.c
+++ process/main.c
@@ -14,11 +14,14 @@
 
 #include "common.h"
 #include "gl.h"
+#include "roadmap.h"
 
 #define WINDOW_RATIO 1
-#define MIXLEVEL 0.7f
+#define MIXLEVEL 0.88f
 
 #define FIFONAME "tmpframefifo.data"
+
+#define RAW_COLORFORMAT gl_texformat_RGBA8
 
 //#define USE_PBO_UPLOAD
 
@@ -146,15 +149,24 @@ int main(int argc, char* argv[]) {
 	unsigned int frameCount = 0;
 
 	/* Program argument check */
-	if (argc != 5) {
-		fputs("Bad arg: Use 'this width height fps color'\n\twhere color = 1(luma), 3(RGB) or 4(RGBA)", stderr);
+	if (argc != 7) {
+		fputs(
+			"Bad arg: Use 'this width height fps color focusRegionFile distanceMapFile'\n"
+			"\twhere color = 1(luma), 3(RGB) or 4(RGBA)\n"
+			"\t      focusRegionFile = Directory to a human-readable file contains focus region\n"
+			"\t      distanceMapFile = Directory to a binary coded file contains road-domain geographic data\n"
+		, stderr);
 		return status;
 	}
 	unsigned int width = atoi(argv[1]), height = atoi(argv[2]), fps = atoi(argv[3]), color = atoi(argv[4]);
-	size2d_t size2d = {.width = width, .height = height};
-	float fsize[2] = {width, height}; //Cast to float, for shader use, this value will never change
-	size_t size1d = width * height; //Total size in pixel, for memory allocation use
-	fprintf(stdout, "Video info = Width: %upx, Height: %upx, FPS: %u, Color: %u\n", width, height, fps, color);
+	const char* focusRegionFile = argv[5];
+	const char* distanceMapFile = argv[6];
+	size2d_t size2d = {.width = width, .height = height}; //Number of pixels in width and height
+	float fsize[2] = {width, height}; //Number of pixels in width[0] and height[1], cast to float for shader use
+	size_t size1d = width * height; //Total number of in pixels = width * height
+	fprintf(stdout, "Video info = Width: %upx, Height: %upx, Total: %zu, FPS: %u, Color: %u\n", width, height, size1d, fps, color);
+	fprintf(stdout, "\tFocus region: %s\n", focusRegionFile);
+	fprintf(stdout, "\tDistance map: %s\n", distanceMapFile);
 
 	if (width & 0b11 || width < 320 || width > 4056) {
 		fputs("Bad width: Width must be multiple of 4, 320 <= width <= 4056\n", stderr);
@@ -184,7 +196,12 @@ int main(int argc, char* argv[]) {
 
 	//Reader thread used to read orginal video raw data from external source
 	pthread_t th_reader_id;
+	struct th_reader_arg arg;
 	int th_reader_idValid = 0;
+
+	//A mesh to store focus region, and a texture texture to store road-domain data
+	gl_mesh mesh_StdRect = GL_INIT_DEFAULT_MESH;
+	gl_tex texture_road = GL_INIT_DEFAULT_TEX;
 
 	//Work buffer: Use off-screen render, one frame buffer as input data and one as output, an extra one to save previous frame
 	gl_fb framebuffer_history = GL_INIT_DEFAULT_FB;
@@ -214,8 +231,6 @@ int main(int argc, char* argv[]) {
 	gl_ubo shader_ubo_gussianMask = GL_INIT_DEFAULT_UBO;
 	gl_ubo shader_ubo_edgeMask = GL_INIT_DEFAULT_UBO;
 
-	gl_mesh mesh_StdRect = GL_INIT_DEFAULT_MESH;
-
 
 
 	/* Init OpenGL and viewer window */ {
@@ -230,7 +245,7 @@ int main(int argc, char* argv[]) {
 		#ifdef USE_PBO_UPLOAD
 			cameraData[0] = gl_pixelBuffer_create(size1d * 4); //Always use RGBA8 (good performance)
 			cameraData[1] = gl_pixelBuffer_create(size1d * 4);
-			if (cameraData[0] == GL_INIT_DEFAULT_PBO || cameraData[1] == GL_INIT_DEFAULT_PBO) {
+			if (!gl_pixelBuffer_check(&(cameraData[0])) || !gl_pixelBuffer_check(&(cameraData[1]))) {
 				fputs("Fail to create pixel buffers for orginal frame data uploading\n", stderr);
 				goto label_exit;
 			}
@@ -243,8 +258,8 @@ int main(int argc, char* argv[]) {
 			}
 		#endif
 
-		texture_orginalBuffer = gl_texture_create(gl_texformat_RGBA8, size2d);
-		if (texture_orginalBuffer == GL_INIT_DEFAULT_TEX) {
+		texture_orginalBuffer = gl_texture_create(RAW_COLORFORMAT, size2d);
+		if (!gl_texture_check(&texture_orginalBuffer)) {
 			fputs("Fail to create texture buffer for orginal frame data storage\n", stderr);
 			goto label_exit;
 		}
@@ -263,7 +278,7 @@ int main(int argc, char* argv[]) {
 		pthread_attr_t attr;
 		pthread_attr_init(&attr);
 		pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-		struct th_reader_arg arg = (struct th_reader_arg){.size = size1d, .colorScheme = color};
+		arg = (struct th_reader_arg){.size = size1d, .colorScheme = color};
 		int err = pthread_create(&th_reader_id, &attr, th_reader, &arg);
 		if (err) {
 			fprintf(stderr, "Fail to init reader thread (%d)\n", err);
@@ -272,18 +287,50 @@ int main(int argc, char* argv[]) {
 		th_reader_idValid = 1;
 	}
 
+	/* Roadmap: mesh for focus region and texture for road info */ {
+		Roadmap roadmap = roadmap_init(focusRegionFile, distanceMapFile, size1d);
+		if (!roadmap) {
+			fputs("Cannot load roadmap\n", stderr);
+			goto label_exit;
+		}
+
+		size_t vCount, iCount;
+		gl_vertex_t* vertices = roadmap_getVertices(roadmap, &vCount);
+		gl_index_t* indices = roadmap_getIndices(roadmap, &iCount);
+		gl_index_t attributes[] = {2};
+		mesh_StdRect = gl_mesh_create((size2d_t){.height=vCount, .width=2}, 3 * iCount, attributes, vertices, indices);
+		if (!gl_mesh_check(&mesh_StdRect)) {
+			roadmap_destroy(roadmap);
+			fputs("Fail to create mesh for roadmap - focus region\n", stderr);
+			goto label_exit;
+		}
+
+		float* geo = roadmap_getGeographic(roadmap);
+		texture_road = gl_texture_create(gl_texformat_RG16F, size2d);
+		if (!gl_texture_check(&texture_road)) {
+			roadmap_destroy(roadmap);
+			fputs("Fail to create texture buffer for roadmap - road-domain data storage\n", stderr);
+			goto label_exit;
+		}
+		gl_texture_update(gl_texformat_RG16F, &texture_road, size2d, geo);
+
+//		gl_fsync();
+		roadmap_destroy(roadmap); //Free roadmap memory after data uploading finished
+
+	}
+
 	/* Create work buffer */ {
-		framebuffer_history = gl_frameBuffer_create(gl_texformat_RGBA8, size2d);
-/*		if (framebuffer_history == GL_INIT_DEFAULT_FB) {
+		framebuffer_history = gl_frameBuffer_create(RAW_COLORFORMAT, size2d);
+		if (!gl_frameBuffer_check(&framebuffer_history)) {
 			fputs("Fail to create frame buffer to store previous frames\n", stderr);
 			goto label_exit;
-		}*/
-		framebuffer_stageA = gl_frameBuffer_create(gl_texformat_RGBA8, size2d);
-		framebuffer_stageB = gl_frameBuffer_create(gl_texformat_RGBA8, size2d);
-/*		if (framebuffer_stageA == GL_INIT_DEFAULT_FB || framebuffer_stageB == GL_INIT_DEFAULT_FB) {
+		}
+		framebuffer_stageA = gl_frameBuffer_create(RAW_COLORFORMAT, size2d);
+		framebuffer_stageB = gl_frameBuffer_create(RAW_COLORFORMAT, size2d);
+		if (!gl_frameBuffer_check(&framebuffer_stageA) || !gl_frameBuffer_check(&framebuffer_stageB)) {
 			fputs("Fail to create work frame buffer A and/or B\n", stderr);
 			goto label_exit;
-		}*/
+		}
 		fb_new = &framebuffer_stageA;
 		fb_old = &framebuffer_stageB;
 		#define swap(a, b) {gl_fb* temp = a; a = b; b = temp;}
@@ -299,7 +346,7 @@ int main(int argc, char* argv[]) {
 		gl_param bId[bCount];
 
 		shader_blit = gl_shader_load("shader/stdRect.vs.glsl", "shader/blit.fs.glsl", 1, pName, pId, pCount, bName, bId, bCount);
-		if (shader_blit == GL_INIT_DEFAULT_SHADER) {
+		if (!gl_shader_check(&shader_blit)) {
 			fputs("Cannot load shader: blit\n", stderr);
 			goto label_exit;
 		}
@@ -320,7 +367,7 @@ int main(int argc, char* argv[]) {
 		gl_param bId[bCount];
 
 		shader_filter3 = gl_shader_load("shader/stdRect.vs.glsl", "shader/filter3.fs.glsl", 1, pName, pId, pCount, bName, bId, bCount);
-		if (shader_filter3 == GL_INIT_DEFAULT_SHADER) {
+		if (!gl_shader_check(&shader_filter3)) {
 			fputs("Cannot load shader: 3*3 filter\n", stderr);
 			goto label_exit;
 		}
@@ -342,7 +389,7 @@ int main(int argc, char* argv[]) {
 		gl_param bId[bCount];
 
 		shader_history = gl_shader_load("shader/stdRect.vs.glsl", "shader/history.fs.glsl", 1, pName, pId, pCount, bName, bId, bCount);
-		if (shader_history == GL_INIT_DEFAULT_SHADER) {
+		if (!gl_shader_check(&shader_history)) {
 			fputs("Cannot load shader: History frame lookback\n", stderr);
 			goto label_exit;
 		}
@@ -361,6 +408,10 @@ int main(int argc, char* argv[]) {
 			1.0f/16,	2.0f/16,	1.0f/16,	0.0f
 		};
 		shader_ubo_gussianMask = gl_uniformBuffer_create(bindingPoint_ubo_gussianMask, sizeof(gussianMask));
+		if (!gl_uniformBuffer_check(&shader_ubo_gussianMask)) {
+			fputs("Cannot load shader UBO: Gussian mask\n", stderr);
+			goto label_exit;
+		}
 		gl_uniformBuffer_update(&shader_ubo_gussianMask, 0, sizeof(gussianMask), gussianMask);
 
 		const float edgeMask[] = {
@@ -369,35 +420,11 @@ int main(int argc, char* argv[]) {
 			1.0f,	1.0f,	1.0f,	0.0f
 		};
 		shader_ubo_edgeMask = gl_uniformBuffer_create(bindingPoint_ubo_edgeMask, sizeof(edgeMask));
-		gl_uniformBuffer_update(&shader_ubo_edgeMask, 0, sizeof(edgeMask), edgeMask);
-	}
-
-	/* Drawing mash (simple rect) */ {
-/*		Roadmap roadmap = roadmap_init(argv[2]);
-		if (!roadmap) {
-			fputs("Cannot load roadmap\n", stderr);
+		if (!gl_uniformBuffer_check(&shader_ubo_edgeMask)) {
+			fputs("Cannot load shader UBO: Edge mask\n", stderr);
 			goto label_exit;
 		}
-
-		size_t vCount, iCount;
-		gl_vertex_t* vertices = roadmap_getVertices(roadmap, &vCount);
-		gl_index_t* indices = roadmap_getIndices(roadmap, &iCount);
-		gl_index_t attributes[] = {2, 2};
-		mesh_StdRect = gl_mesh_create((size2d_t){.height=vCount, .width=4}, 3 * iCount, attributes, vertices, indices);
-
-		gl_fsync();
-		roadmap_destroy(roadmap); //Free roadmap memory after data uploading finished*/
-
-		gl_vertex_t vertices[] = {
-			1.0f, 1.0f,
-			1.0f, 0.0f,
-			0.0f, 0.0f,
-			0.0f, 1.0f
-		};
-		gl_index_t attributes[] = {2};
-		gl_index_t indices[] = {0, 3, 2, 0, 2, 1};
-		mesh_StdRect = gl_mesh_create((size2d_t){.height=4, .width=2}, 6, attributes, vertices, indices);
-
+		gl_uniformBuffer_update(&shader_ubo_edgeMask, 0, sizeof(edgeMask), edgeMask);
 	}
 
 	
@@ -417,10 +444,10 @@ int main(int argc, char* argv[]) {
 		
 		/* Give next frame PBO to reader while using current PBO for rendering */
 		#ifdef USE_PBO_UPLOAD
-			gl_pixelBuffer_updateToTexture(gl_texformat_RGBA8, &cameraData[ (frameCount+0)&1 ], &texture_orginalBuffer, size2d); //Current frame PBO (index = +0) <-- Data already in GPU, this operation is fast
+			gl_pixelBuffer_updateToTexture(RAW_COLORFORMAT, &cameraData[ (frameCount+0)&1 ], &texture_orginalBuffer, size2d); //Current frame PBO (index = +0) <-- Data already in GPU, this operation is fast
 			rawDataPtr = gl_pixelBuffer_updateStart(&cameraData[ (frameCount+1)&1 ], size1d * 4); //Next frame PBO (index = +1)
 		#else
-			gl_texture_update(gl_texformat_RGBA8, &texture_orginalBuffer, size2d, rawData[ (frameCount+0)&1 ]);
+			gl_texture_update(RAW_COLORFORMAT, &texture_orginalBuffer, size2d, rawData[ (frameCount+0)&1 ]);
 			rawDataPtr = rawData[ (frameCount+1)&1 ];
 		#endif
 		sem_post(&sem_readerJobStart); //New GPU address ready, start to reader thread
@@ -478,7 +505,6 @@ int main(int argc, char* argv[]) {
 	/* Free all resources, house keeping */
 	status = EXIT_SUCCESS;
 label_exit:
-	gl_mesh_delete(&mesh_StdRect);
 	
 	gl_unifromBuffer_delete(&shader_ubo_gussianMask);
 	gl_unifromBuffer_delete(&shader_ubo_edgeMask);
@@ -491,6 +517,9 @@ label_exit:
 	gl_frameBuffer_delete(&framebuffer_stageB);
 	gl_frameBuffer_delete(&framebuffer_stageA);
 	gl_frameBuffer_delete(&framebuffer_history);
+
+	gl_texture_delete(&texture_road);
+	gl_mesh_delete(&mesh_StdRect);
 
 	if (th_reader_idValid) {
 		pthread_cancel(th_reader_id);
