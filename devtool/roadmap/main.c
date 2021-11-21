aeb4c74ff0f0268ceeb458902b2c64b00410baab
Captdam <captdam@beardle.com>
2021-11-20 23:55:42 -0500

    Update dev tool - roadmap gen: - Generate projection mesh - New meta data in road map

--- devtool/roadmap/main.c
+++ devtool/roadmap/main.c
@@ -1,8 +1,8 @@
-/** This program is used to generate road map file. 
+/** This program is used to generate road map file and project file. 
  * A road map file contains infomation about the road-domain geographic data of each pixel. 
- * For example, if an object is located at a pixel at screen-domain (pixel-x,pixely). 
- * Using this data, we can get that object is located at a point on the road (geo-a-meter,geo-b-meter). 
- * Format: 1-D plain array of struct {float geo-x, float geo-y} [size = width * height]. 
+ * For example, if an object is located at a pixel at screen-domain (pixel-x-px, pixel-y-px). 
+ * Using this data, we can get that object is on the road-domain (geo-x-meter, geo-y-meter). 
+ * Format: array of struct {float geo-x-meter, float geo-y-meter, float-se} [height][width]. 
  */
 
 #include <stdio.h>
@@ -10,6 +10,11 @@
 #include <math.h>
 #include <inttypes.h>
 #include <errno.h>
+
+typedef struct Point_t {
+	double roadX, roadY;
+	unsigned int screenX, screenY;
+} point_t;
 
 double d2r(double deg) {
 	return deg * M_PI / 180;
@@ -25,9 +30,9 @@ double fd(double x1, double y1, double x2, double y2) {
 }
 
 int main(int argc, char* argv[]) {
-	if (argc != 10) {
+	if (argc != 11) {
 		fputs(
-			"Bad arg: Use 'this installHeight roadPitch installPitch fovHorizontal fovVertical sizeHorizontal sizeVeritcal threshold file'\n"
+			"Bad arg: Use 'this installHeight roadPitch installPitch fovHorizontal fovVertical sizeHorizontal sizeVeritcal threshold fileRoad fileProj'\n"
 			"\twhere installHeight is height of camera in meter\n"
 			"\t      roadPitch is pitch angle of road in degree, incline is positive\n"
 			"\t      installPitch is pitch angle of camera in degree, looking sky is positive\n"
@@ -36,28 +41,34 @@ int main(int argc, char* argv[]) {
 			"\t      sizeHorizontal is width of camera image in unit of pixel\n"
 			"\t      sizeVeritcal is height of camera image in unit of pixel\n"
 			"\t      threshold is the max displacement an object can have between two frame in unit of meter\n"
-			"\t      file is where to save the roadmap file\n"
-			"\teg; this 10.0 20.0 0.0 57.5 32.3 1920 1080 3.5 map.data\n"
+			"\t      fileRoad is where to save the road map file\n"
+			"\t      fileProj is where to save the project file\n"
+			"\teg; this 10.0 20.0 0.0 57.5 32.3 1920 1080 3.5 map.data project.txt\n"
 		, stderr);
 		return EXIT_FAILURE;
 	}
 	double installHeight = atof(argv[1]), roadPitch = atof(argv[2]), installPitch = atof(argv[3]), fovHorizontal = atof(argv[4]), fovVertical = atof(argv[5]);
 	unsigned int width = atoi(argv[6]), height = atoi(argv[7]);
 	double threshold = atof(argv[8]);
-	const char* fn = argv[9];
+	const char* roadmap = argv[9];
+	const char* project = argv[10];
 	fprintf(stdout, "Camera height: %lf m, pitch: %lf deg\n", installHeight, installPitch);
 	fprintf(stdout, "Camera resolution: %u x %u p, FOV: %lf x%lf deg\n", width, height, fovHorizontal, fovVertical);
 	fprintf(stdout, "Road picth: %lf deg\n", roadPitch);
 	fprintf(stdout, "Threshold: %lf m/frame\n", threshold);
-	fprintf(stdout, "Genarating roadmap to file '%s'\n", fn);
 	fflush(stdout);
 
-	FILE* fp = fopen(fn, "wb");
+	FILE* fp;
+	point_t farLeft, farRight, closeLeft, closeRight;
+	
+	fprintf(stdout, "Genarating road map file '%s'\n", roadmap);
+	fflush(stdout);
+
+	fp = fopen(roadmap, "wb");
 	if (!fp) {
-		fprintf(stderr, "Fail to create/open output file '%s' (errno=%d)\n", fn, errno);
+		fprintf(stderr, "Fail to create/open output file '%s' (errno=%d)\n", roadmap, errno);
 		return EXIT_FAILURE;
 	}
-
 	float (* buffer)[width][4] = malloc(sizeof(float) * height * width * 4);
 	if (!buffer) {
 		fprintf(stderr, "Out of memory (errno=%d)\n", errno);
@@ -79,9 +90,25 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
+	double lastY = 0.0;
 	for (unsigned int y = 0; y < height; y++) {
+		double cy = buffer[y][0][1]; //Current position; if above horizon, cy will be negative
+		int isFar = lastY < 0 && cy > 0;
+		int isClose = y == height - 1;
 		for (unsigned int x = 0; x < width; x++) {
-			double cx = buffer[y][x][0], cy = buffer[y][x][1]; //Current position
+			double cx = buffer[y][x][0];
+
+			if (isFar) {
+				if (x == 0)
+					farLeft = (point_t){.screenX = x, .screenY = y, .roadX = cx, .roadY = cy};
+				else if (x == width - 1)
+					farRight = (point_t){.screenX = x, .screenY = y, .roadX = cx, .roadY = cy};
+			} else if (isClose) {
+				if (x == 0)
+					closeLeft = (point_t){.screenX = x, .screenY = y, .roadX = cx, .roadY = cy};
+				else if (x == width - 1)
+					closeRight = (point_t){.screenX = x, .screenY = y, .roadX = cx, .roadY = cy};;
+			}
 			
 			unsigned int left = 0;
 			while (1) {
@@ -118,8 +145,9 @@ int main(int argc, char* argv[]) {
 			buffer[y][x][2] = fmax(left, right);
 			buffer[y][x][3] = fmax(up, down);
 
-	//		fprintf(stdout, "Point (%u,%u) - position %f %f, search region %f %f.\n", x, y, buffer[y][x][0], buffer[y][x][1], buffer[y][x][2], buffer[y][x][3]);
+//			fprintf(stdout, "Point (%u,%u) - position %f %f, search region %f %f.\n", x, y, buffer[y][x][0], buffer[y][x][1], buffer[y][x][2], buffer[y][x][3]);
 		}
+		lastY = cy;
 	}
 
 	fwrite(buffer, sizeof(float), height * width * 4, fp);
@@ -127,13 +155,45 @@ int main(int argc, char* argv[]) {
 	fwrite(&max, sizeof(float), 1, fp);
 
 	fputs("\n\n== Metadata: ===================================================================\n", fp);
+	fprintf(fp, "CMD: %s %s %s %s %s %s %s %s\n", argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], argv[7], argv[8]);
 	fprintf(fp, "Camera height: %lf m, pitch: %lf deg\n", installHeight, installPitch);
 	fprintf(fp, "Camera resolution: %u x %u p, FOV: %lf x %lf deg\n", width, height, fovHorizontal, fovVertical);
 	fprintf(fp, "Road picth: %lf deg\n", roadPitch);
 	fprintf(fp, "Threshold: %lf m/frame\n", threshold);
-	fprintf(fp, "Genarating roadmap to file '%s'\n", fn);
+	fclose(fp);
+
+	fprintf(stdout, "Road map file '%s' generated\n", roadmap);
+	fflush(stdout);
+
+	fprintf(stdout, "Genarating project file '%s'\n", roadmap);
+	fprintf(stdout, "Far left:    screen(%u, %u), road(%lf, %lf)\n", farLeft.screenX, farLeft.screenY, farLeft.roadX, farLeft.roadY);
+	fprintf(stdout, "Far right:   screen(%u, %u), road(%lf, %lf)\n", farRight.screenX, farRight.screenY, farRight.roadX, farRight.roadY);
+	fprintf(stdout, "Close left:  screen(%u, %u), road(%lf, %lf)\n", closeLeft.screenX, closeLeft.screenY, closeLeft.roadX, closeLeft.roadY);
+	fprintf(stdout, "Close right: screen(%u, %u), road(%lf, %lf)\n", closeRight.screenX, closeRight.screenY, closeRight.roadX, closeRight.roadY);
+	fflush(stdout);
+
+	fp = fopen(project, "wb");
+	if (!fp) {
+		fprintf(stderr, "Fail to create/open output file '%s' (errno=%d)\n", project, errno);
+		return EXIT_FAILURE;
+	}
+
+	double closeLeft2FarX = (closeLeft.roadX - farLeft.roadX) / (farRight.roadX - farLeft.roadX);
+	double closeRight2FarX = (closeRight.roadX - farLeft.roadX) / (farRight.roadX - farLeft.roadX);
+	double farY = farLeft.screenY / (double)height;
+	/*                                      Proj-X			Proj-Y	Ortho-X	Orth-Y */
+	fprintf(fp, "v\t%lf\t%lf\t%lf\t%lf\n",	closeLeft2FarX,		farY,	0.0,	farY); //LT
+	fprintf(fp, "v\t%lf\t%lf\t%lf\t%lf\n",	closeRight2FarX,	farY,	1.0,	farY); //RT
+	fprintf(fp, "v\t%lf\t%lf\t%lf\t%lf\n",	0.0,			1.0,	0.0,	1.0);  //LB
+	fprintf(fp, "v\t%lf\t%lf\t%lf\t%lf\n",	1.0,			1.0,	1.0,	1.0);  //RB
+	fputs("i\t0\t1\t2\n", fp);
+	fputs("i\t1\t3\t2\n", fp);
 
 	fclose(fp);
+
+	fprintf(stdout, "Project file '%s' generated\n", project);
+	fflush(stdout);
+
 	fputs("Done!\n", stdout);
 	return EXIT_SUCCESS;
 }
