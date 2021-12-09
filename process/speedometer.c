565110fdaa4ff265b49f97a8f1267639a9cdd560
Captdam <captdam@beardle.com>
2021-12-09 15:39:28 -0500

    Update speedometer lib: - Remove vertices and indices processing inside class - Vertices/indices progessing now handled by main program - flex++

--- process/speedometer.c
+++ process/speedometer.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <inttypes.h>
+#include <string.h>
 
 #ifdef VERBOSE
 #include <errno.h>
@@ -11,12 +12,9 @@
 struct Speedometer_ClassDataStructure {
 	size2d_t glyphSize;
 	uint32_t* glyph;
-	float* vertices;
-	unsigned int* indices;
-	size2d_t count;
 };
 
-Speedometer speedometer_init(const char* glyphs, size2d_t count) {
+Speedometer speedometer_init(const char* glyphs) {
 	#ifdef VERBOSE
 		fputs("Init speedometer class object\n", stdout);
 		fflush(stdout);
@@ -31,105 +29,83 @@ Speedometer speedometer_init(const char* glyphs, size2d_t count) {
 	}
 	*this = (struct Speedometer_ClassDataStructure){
 		.glyphSize = {0, 0},
-		.glyph = NULL,
-		.vertices = NULL,
-		.indices = NULL,
-		.count = {0, 0}
+		.glyph = NULL
 	};
 
-	/* Load glyph from file */ {
-		FILE* fp = fopen(glyphs, "rb");
-		if (!fp) {
-			#ifdef VERBOSE
-				fprintf(stderr, "Fail to open speedometer bitmap file: %s (errno = %d)\n", glyphs, errno);
-			#endif
-			speedometer_destroy(this);
-			return NULL;
-		}
-
-		uint8_t glyphSize[2]; //Use 2 * 1 bytes in file
-		if (!fread(glyphSize, sizeof(uint8_t), 2, fp)) {
-			#ifdef VERBOSE
-				fputs("Fail to read speedometer glyph size from file\n", stderr);
-			#endif
-			fclose(fp);
-			speedometer_destroy(this);
-			return NULL;
-		}
-		this->glyphSize = (size2d_t){.width = glyphSize[0], .height = glyphSize[1]}; //Cast from uint8_t[2]
-
-		this->glyph = malloc(256 * this->glyphSize.width * this->glyphSize.height * sizeof(uint32_t));
-		if (!this->glyph) {
-			#ifdef VERBOSE
-				fputs("Fail to create buffer for speedometer glyph\n", stderr);
-			#endif
-			speedometer_destroy(this);
-			return NULL;
-		}
-
-		if (!fread(this->glyph, sizeof(uint32_t), 256 * this->glyphSize.width * this->glyphSize.height, fp)) {
-			#ifdef VERBOSE
-				fputs("Fail to read speedometer glyph from file\n", stderr);
-			#endif
-			fclose(fp);
-			speedometer_destroy(this);
-			return NULL;
-		}
-
-		fclose(fp);
+	FILE* fp = fopen(glyphs, "rb");
+	if (!fp) {
+		#ifdef VERBOSE
+			fprintf(stderr, "Fail to open speedometer bitmap file: %s (errno = %d)\n", glyphs, errno);
+		#endif
+		speedometer_destroy(this);
+		return NULL;
 	}
 
-	/* Generate location and sample region of each speedometer base on the given param 'count' (number of meters) */ {
-		this->vertices = malloc(count.width * count.height * 4 * sizeof(float));
-		this->indices = malloc(count.width * count.height * sizeof(unsigned int));
-		if (!this->vertices || !this->indices) {
-			#ifdef VERBOSE
-				fputs("Fail to create buffer for speedometer vertices and indices\n", stderr);
-			#endif
-			speedometer_destroy(this);
-			return NULL;
-		}
+	uint8_t glyphSize[2]; //Use 2 * 1 bytes in file
+	if (!fread(glyphSize, sizeof(uint8_t), 2, fp)) {
+		#ifdef VERBOSE
+			fputs("Fail to read speedometer glyph size from file\n", stderr);
+		#endif
+		fclose(fp);
+		speedometer_destroy(this);
+		return NULL;
+	}
+	this->glyphSize = (size2d_t){.width = glyphSize[0], .height = glyphSize[1]}; //Cast from uint8_t[2]
 
-		/** Note: Why not use instanced draw and geometry shader?
-		 * For compatibility and simplicity, we use the most basic method to draw the speedometer. 
-		 * - Instanced draw requires a new vbo, means we have to modify the orginal gl lib. 
-		 * - Instanced draw is introduced in GLES3.1 core, old GPU/driver will not support it, new driver may emulate it. 
-		 * - Geometry shader is introduced in GLES3.1 core, old GPU/driver will not support it, new driver may emulate it. 
-		 * - Our speedometer is small, we only have few vertices, standard draw can handle this load well. 
-		 */
+	this->glyph = malloc(256 * this->glyphSize.width * this->glyphSize.height * sizeof(uint32_t));
+	if (!this->glyph) {
+		#ifdef VERBOSE
+			fputs("Fail to create buffer for speedometer glyph\n", stderr);
+		#endif
+		speedometer_destroy(this);
+		return NULL;
+	}
 
-		this->count = count;
-		float sizeH = 1.0 / count.width, sizeV = 1.0 / count.height;
-		float* vptr = this->vertices;
-		unsigned int* iptr = this->indices;
-		unsigned int idx = 0;
-		for (unsigned int y = 0; y < count.height; y++) {
-			for (unsigned int x = 0; x < count.width; x++) {
-				*(vptr++) = sizeH * x;		//Left limit (p1x)
-				*(vptr++) = sizeV * y;		//Top limit (p1y)
-				*(vptr++) = sizeH * x + sizeH;	//Right limit (p2x)
-				*(vptr++) = sizeV * y + sizeV;	//Bottom limit (p2y)
-				*(iptr++) = idx;
+	if (!fread(this->glyph, sizeof(uint32_t), 256 * this->glyphSize.width * this->glyphSize.height, fp)) {
+		#ifdef VERBOSE
+			fputs("Fail to read speedometer glyph from file\n", stderr);
+		#endif
+		fclose(fp);
+		speedometer_destroy(this);
+		return NULL;
+	}
+
+	fclose(fp);
+	return this;
+}
+
+int speedometer_convert(Speedometer this) {
+	size2d_t size = this->glyphSize;
+	size_t totalPixel = 256 * size.width * size.height;
+	size_t totalSize = totalPixel * sizeof(uint32_t);
+
+	uint32_t (*temp)[size.height][size.width] = malloc(256 * size.width * size.height * sizeof(uint32_t));
+	if (!temp) {
+		return 0;
+	}
+
+	for (unsigned int oy = 0; oy < 16; oy++) {
+		for (unsigned int ox = 0; ox < 16; ox++) {
+			for (unsigned int iy = 0; iy < size.height; iy++) {
+				for (unsigned int ix = 0; ix < size.width; ix++) {
+					unsigned int newIdx = oy * 16 + ox;
+					unsigned int oldWidth = size.width * 16;
+					unsigned int oldOffsetV = oy * size.height;
+					unsigned int oldOffsetH = ox * size.width;
+					temp[newIdx][iy][ix] = this->glyph[ (oldOffsetV + iy) * oldWidth + (oldOffsetH + ix)];
+				}
 			}
 		}
 	}
 
-	return this;
+	memcpy(this->glyph, temp, 256 * size.width * size.height * sizeof(uint32_t));
+	free(temp);
+	return 1;
 }
 
 uint32_t* speedometer_getGlyph(Speedometer this, size2d_t* size) {
 	*size = this->glyphSize;
 	return this->glyph;
-}
-
-float* speedometer_getVertices(Speedometer this, size_t* size) {
-	*size = this->count.width * this->count.height;
-	return this->vertices;
-}
-
-unsigned int* speedometer_getIndices(Speedometer this, size_t* size) {
-	*size = this->count.width * this->count.height;
-	return this->indices;
 }
 
 void speedometer_destroy(Speedometer this) {
@@ -143,10 +119,6 @@ void speedometer_destroy(Speedometer this) {
 
 	if (this->glyph)
 		free(this->glyph);
-	if (this->vertices)
-		free(this->vertices);
-	if (this->indices)
-		free(this->indices);
 
 	free(this);
 }
--- process/speedometer.h
+++ process/speedometer.h
@@ -12,15 +12,23 @@
  */
 typedef struct Speedometer_ClassDataStructure* Speedometer;
 
-/** Init a speedometer object, allocate memory, read speedometer glyph from bitmap file, init mesh. 
+/** Init a speedometer object, allocate memory, read speedometer glyph from bitmap file. 
  * The glyph bitmap file must contains 256 glyphs, 16 rows in total, each row contains 16 glyphs. This bitmap must be in RGBA8 (uint32_t) format. 
  * There is no restriction on the size of the glyph, but all of them should have same size. 
- * The whole frame is divided into a number of rectangular speedometer, count.x in horizontal direction, count.y in vertical direction. 
  * @param glyphs Directory to a glyphs file, must be in plain RGBA8 raw data format
- * @param count Number of speedometer in horizontal and vertical direction
  * @return $this(Opaque) speedometer class object upon success. If fail, free all resource and return NULL
  */
-Speedometer speedometer_init(const char* glyphs, size2d_t count);
+Speedometer speedometer_init(const char* glyphs);
+
+/** Convert the glyph to array. 
+ * By default, the glyph map layout is 16 * 16; in another word, a flat texture contains 256 small texture. This works for 2D texture. 
+ * By calling this method, the flat 2D glyph map will be converted into 2D array texture; which means, there will be 256 different 2D textures in the array. 
+ * Each of the 256 glyph can be access by using index (z-axis). This works for 2D array texture. 
+ * @param this This speedometer class object
+ * @return On success, return 1; if fail (out of memory), return 0
+ */
+int speedometer_convert(Speedometer this);
+
 
 /** Get a pointer to the glyph loaded from file, which can be upload to GPU as texture. 
  * This 2D bitmap contains a collection of 256 glpphs. 16 glyphs in one row, and there are 16 rows. 
@@ -30,25 +38,6 @@ Speedometer speedometer_init(const char* glyphs, size2d_t count);
  * @return Pointer to the bitmap glyph (format = RGBA8, uint32_t)
  */
 uint32_t* speedometer_getGlyph(Speedometer this, size2d_t* size);
-
-/** Get a pointer to the vertices array of the speedometer. 
- * A vertex contains 4 float value: left-x, top-y, right-x and bottom-y coordinates of each rectangular speedometer. 
- * Which is: {{left-x, top-y, right-x, bottom-y}, ...}. 
- * Should use gl_points. 
- * @param this This speedometer class object
- * @param size Pass by reference: Number of vertex (1 vertex = 4 float), should pass (count.x * count.y) back
- * @return Pointer to vertex array
- */
-float* speedometer_getVertices(Speedometer this, size_t* size);
-
-/** Get a pointer to the indices array of the speedometer. 
- * Each speedometer only uses 1 vertex; hence the indices is just an array of number in row. 
- * An index contains 1 unsigned int, representing the index of 1 vertice in the vertices array which form a point. 
- * @param this This speedometer class object
- * @param size Pass by reference: Number of index (1 index = 1 uint), should pass (count.x * count.y) back
- * @return Pointer to indices array
- */
-unsigned int* speedometer_getIndices(Speedometer this, size_t* size);
 
 /** Destroy this speedometer class object, frees resources. 
  * @param this This speedometer class object
