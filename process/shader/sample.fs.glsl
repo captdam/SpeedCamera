c56a9ec52d7b5ed320e33d777e838b5d75ae0edf
Captdam <captdam@beardle.com>
2022-04-19 17:36:07 -0400

    Update sampler shader: Add min sample size

--- process/shader/sample.fs.glsl
+++ process/shader/sample.fs.glsl
@@ -1,11 +1,14 @@
 in highp vec2 pxPos;
-out mediump float result; //Data, speed
+out mediump vec2 result; //Data: vec2(speed, target_yCoord)
+#define CH_ROADDISTANCE x
+#define CH_TARGETYCOORD y
 
-uniform mediump sampler2D src; //Data, speed
+uniform mediump sampler2D src; //Data: vec2(speed, target_yCoord)
+
+/* Defined by client: MIN_SAMPLE_SIZE float */
 
 #define SEARCH_TOTAL x
 #define SEARCH_COUNT y
-
 highp vec2 search(highp sampler2D map, highp float threshold, mediump ivec2 start, lowp int dir) {
 	highp vec2 res = vec2(0.0); //Accumulation, use highp
 	mediump ivec2 idx = start;
@@ -14,7 +17,7 @@ highp vec2 search(highp sampler2D map, highp float threshold, mediump ivec2 star
 		highp float value;
 
 		dest = idx + ivec2(dir, 0); //Middle
-		value = texelFetch(map, dest, 0).r;
+		value = texelFetch(map, dest, 0).CH_ROADDISTANCE;
 		if (value > threshold) {
 			idx = dest;
 			res.SEARCH_TOTAL += value;
@@ -23,7 +26,7 @@ highp vec2 search(highp sampler2D map, highp float threshold, mediump ivec2 star
 		}
 
 		dest = idx + ivec2(dir, -1); //Up
-		value = texelFetch(map, dest, 0).r;
+		value = texelFetch(map, dest, 0).CH_ROADDISTANCE;
 		if (value > threshold) {
 			idx = dest;
 			res.SEARCH_TOTAL += value;
@@ -32,7 +35,7 @@ highp vec2 search(highp sampler2D map, highp float threshold, mediump ivec2 star
 		}
 
 		dest = idx + ivec2(dir, +1); //Down
-		value = texelFetch(map, dest, 0).r;
+		value = texelFetch(map, dest, 0).CH_ROADDISTANCE;
 		if (value > threshold) {
 			idx = dest;
 			res.SEARCH_TOTAL += value;
@@ -48,16 +51,19 @@ highp vec2 search(highp sampler2D map, highp float threshold, mediump ivec2 star
 void main() {
 	mediump ivec2 pxIdx = ivec2(vec2(textureSize(src, 0)) * pxPos);
 
-	highp float res = 0.0;
+	mediump vec2 res = vec2(0.0);
 
-	if (texelFetch(src, pxIdx, 0).r > 0.0) {
+	mediump vec4 current = texelFetch(src, pxIdx, 0);
+	if (current.CH_ROADDISTANCE > 0.0) {
+
 		highp vec2 left = search(src, 0.0, pxIdx, -1);
 		highp vec2 right = search(src, 0.0, pxIdx, +1);
 
-		if (abs(0.5 + left.SEARCH_COUNT - right.SEARCH_COUNT) < 1.0) { //Get center point, left == right or left == right - 1
+		if ( min(left.SEARCH_COUNT, right.SEARCH_COUNT) > MIN_SAMPLE_SIZE && abs(0.5 + left.SEARCH_COUNT - right.SEARCH_COUNT) < 1.0 ) { //Remove small sample (noise); Get center point, left == right or left == right - 1
 			highp float total = left.SEARCH_TOTAL + right.SEARCH_TOTAL;
 			highp float count = left.SEARCH_COUNT + right.SEARCH_COUNT;
-			res = total / count;
+			res.CH_ROADDISTANCE = total / count;
+			res.CH_TARGETYCOORD = current.CH_TARGETYCOORD;
 		}
 	}
 
